9b5205cb15307ff3fa81c1b0e8b6694ae1d40171
quoctuan1998 <quoctuanb6@gmail.com>
2018-02-09 12:22:20 +0700

    update listen music xml

--- app/src/main/java/com/example/quoctuan/mymusic/ListenMusicActivity.java
+++ app/src/main/java/com/example/quoctuan/mymusic/ListenMusicActivity.java
@@ -3,7 +3,7 @@ package com.example.quoctuan.mymusic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListenMusicActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
